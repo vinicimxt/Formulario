05e4eb0043b21de0459d9814a977475f72b97400
unknown <vinicius.generoso2@ete.sp.gov.br>
2023-10-10 20:40:17 -0300

    java

---
+++ aula 78/javascript.js
@@ -0,0 +1,12 @@
+let nome //let é uma variável(não e necessário informar o tipo)
+
+
+nome="josney"//atributo da variável
+
+
+console.log(nome)//exibe o valor armazenado na variavel nome no console do navegador (josney)
+
+
+window.alert('minha primeira mensagem!') // pop up com informação e botão ok
+window.confirm('está gostando do site?') //abre pop up com opção ok ou cancelar
+window.prompt('escreve aqui teu nome') //abre pop up pra digitar o nome
